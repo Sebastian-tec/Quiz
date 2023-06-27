651c84ed8c5192f7bd927ba1c6d7e0b58a3e79ed
Sebastian-tec <seba9188@elev.tec.dk>
2023-06-27 12:16:13 +0200

    Init commit

---
+++ ToJSON/JsonParser.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ToJSON
+{
+    internal class JsonParser
+    {
+        public static List<Quiz> Quiz()
+        {
+            string file = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory() + "\\quiz.json"));
+            List<Quiz> quiz = JsonConvert.DeserializeObject<List<Quiz>>(file);
+            
+            return quiz;
+        }
+    }
+
+    public class Quiz
+    {
+        public string Question { get; set; }
+        public List<string> Answers = new List<string>();
+        public int Correct { get; set; }
+        public string Description { get; set; }
+
+        public Quiz(string question, List<string> answers, int correct, string desc)
+        {
+            Question = question;
+            Answers = answers;
+            Correct = correct;
+            Description = desc;
+        }
+    }
+}
---
+++ ToJSON/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.AccessControl;
+
+namespace ToJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            List<Quiz> list = JsonParser.Quiz();
+
+            foreach (Quiz item in list)
+            {
+
+                int choice = 0;
+                int counter = 0;
+                do
+                {
+                    Console.WriteLine(item.Question);
+                    foreach (var answer in item.Answers)
+                    {
+                        
+                        Console.WriteLine($"[{counter}] " + answer);
+                        counter++;
+                    }
+                    Console.Write("Enter your choice: ");
+
+                } while (!int.TryParse(Console.ReadLine(), out choice) && choice < 4);
+                string result = (item.Answers[choice] == item.Answers[item.Correct]) ? "Correct answer!" : $"Wrong answer! The right answer is {item.Answers[item.Correct]}";
+                Console.WriteLine($"\n{result}\n{item.Description}\n");
+               
+            }
+        }
+    }
+
+
+}
